1b6e1a948d515267d2edddc0db2a838b941200ff
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-12 12:12:52 -0400

    Update Javaprac.js

--- Javaprac.js
+++ Javaprac.js
@@ -1,7 +1,15 @@
+/*
+PROGRAM: User Input Alert Script
+This JavaScript program retrieves text entered
+by the user from an HTML form input field and
+displays the entered value using an alert message
+*/
+
 "use strict";
 
+/* ---- TEST RESULTS FUNCTION: Retrieves user input from the form text box and displays the entered value in an alert box ---- */
 function testResults(form) {
-  var TestVar = form.inputbox.value;
-  alert("You typed: " + TestVar);
+  var TestVar = form.inputbox.value; // Store user input value from form field
+  alert("You typed: " + TestVar); // Display entered value in alert message
 
 }
